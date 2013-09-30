c2cd341993a62cdf75e1a0b3d727cf7d8c9c0c28
Jens Werner Nielsen <jenswernernielsen@gmail.com>
2013-09-30 16:06:20 +0200

    User

--- 02334/src/Main.java
+++ 02334/src/Main.java
@@ -1,26 +1,33 @@
+import java.util.List;
+
 import controller.Boundary;
 import database.Connector;
+import database.User;
 
 public class Main {
 
 	public static void main(String[] args) {	
 		
+		Connector connector = null;
+		
 		try {
-			Connector connector = new Connector("sql-lab1.cc.dtu.dk", 3306, "s123115", "s123115", "F5iCtVPs4rtHu4oM");
+			
+			connector = new Connector("sql-lab1.cc.dtu.dk", 3306, "s123115", "s123115", "F5iCtVPs4rtHu4oM");
 			connector.reset();
-			connector.close();
+			
+			List<User> users = connector.getUsers();
+			
+			System.out.println("Users:");
+			System.out.println();
+			for (User user : users) {
+				System.out.println(" " + user.getIdentifier() + "   " + user.getMail() + "   " + user.getName() + "   " + user.getType());
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			connector.close();
 		}
-		
-		//tester her
-		
-		Boundary.passCheck("1222");
-		Boundary.passCheck("1223");
-		
-		
-		
-		
 		
 	}
 
--- 02334/src/database/Connector.java
+++ 02334/src/database/Connector.java
@@ -3,14 +3,17 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Connector {
 	
 	// ############################# new Connector("sql-lab1.cc.dtu.dk", 3306, "s123115", "s123115", "F5iCtVPs4rtHu4oM")
 	
 	private Connection connection;
-	private Statement statement;
 	
 	// New
 	
@@ -18,7 +21,6 @@ public class Connector {
 		
 		try {
 			connection = DriverManager.getConnection("jdbc:mysql://" + server + ":" + port + "/" + database, username, password);
-			statement = connection.createStatement();
 		} catch (Exception ex) {
 			close();
 			throw ex;
@@ -31,10 +33,6 @@ public class Connector {
 	public void close() {
 
 		try {
-			statement.close();
-		} catch (Exception ex) {
-		}
-		try {
 			connection.close();
 		} catch (Exception ex) {
 		}
@@ -45,78 +43,117 @@ public class Connector {
 	
 	public void reset() throws Exception {
 
-		// Drop
+		Statement statement = null;
+		
+		try {
 
-		try {
-			statement.executeUpdate("DROP TABLE comments;");
-		} catch (Exception ex) {
-		}
-		try {
-			statement.executeUpdate("DROP TABLE threads;");
-		} catch (Exception ex) {
-		}
-		try {
-			statement.executeUpdate("DROP TABLE categories;");
-		} catch (Exception ex) {
-		}
-		try {
-			statement.executeUpdate("DROP TABLE users;");
-		} catch (Exception ex) {
-		}
+			statement = connection.createStatement();
+		
+			// Drop
 
-		// Create
+			try {
+				statement.executeUpdate("DROP TABLE comments;");
+			} catch (Exception e) {
+			}
+			try {
+				statement.executeUpdate("DROP TABLE threads;");
+			} catch (Exception e) {
+			}
+			try {
+				statement.executeUpdate("DROP TABLE categories;");
+			} catch (Exception e) {
+			}
+			try {
+				statement.executeUpdate("DROP TABLE users;");
+			} catch (Exception e) {
+			}
+
+			// Create
+			
+			statement.executeUpdate("CREATE TABLE users (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
+														"mail VARCHAR(50) NOT NULL UNIQUE, " +
+														"name VARCHAR(50) NOT NULL UNIQUE, " +
+														"password VARBINARY(32) NOT NULL, " +
+														"type INTEGER NOT NULL, " +
+														"PRIMARY KEY (identifier));");
+			
+			statement.executeUpdate("CREATE TABLE categories (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
+															 "name VARCHAR(100) NOT NULL, " +
+															 "parent INTEGER, " +
+															 "PRIMARY KEY (identifier), " +
+															 "UNIQUE (name, parent), " +
+															 "FOREIGN KEY (parent) REFERENCES categories(identifier));");
+			
+			statement.executeUpdate("CREATE TABLE threads (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
+														  "user INTEGER NOT NULL, " +
+														  "category INTEGER NOT NULL, " + 
+														  "name VARCHAR(100) NOT NULL, " +
+														  "sticky BOOLEAN NOT NULL, " +
+														  "closed BOOLEAN NOT NULL, " +
+														  "created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+														  "PRIMARY KEY (identifier), " +
+														  "FOREIGN KEY (user) REFERENCES users(identifier), " +
+														  "FOREIGN KEY (category) REFERENCES categories(identifier));");
+			
+			statement.executeUpdate("CREATE TABLE comments (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
+														   "user INTEGER NOT NULL, " +
+														   "thread INTEGER NOT NULL, " +
+														   "content VARCHAR(1000) NOT NULL, " +
+														   "changed TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
+														   "PRIMARY KEY (identifier), " +
+														   "FOREIGN KEY (user) REFERENCES users(identifier), " +
+														   "FOREIGN KEY (thread) REFERENCES threads(identifier));");
+			
+			// Insert		
+			
+			statement.executeUpdate("INSERT INTO users (mail, name, password, type) VALUES ('administrator@test.com', 'Administrator', 'a', 0);"); // TODO: Ændre password til hash!
+			statement.executeUpdate("INSERT INTO users (mail, name, password, type) VALUES ('moderator@test.com', 'Moderator', 'm', 1);"); // TODO: Ændre password til hash!
+			statement.executeUpdate("INSERT INTO users (mail, name, password, type) VALUES ('bruger@test.com', 'Bruger', 'b', 2);"); // TODO: Ændre password til hash!
+			
+			statement.executeUpdate("INSERT INTO categories (name, parent) VALUES ('Hovedkategori', NULL);");
+			statement.executeUpdate("INSERT INTO categories (name, parent) VALUES ('Underkategori', 1);");
+			
+			statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (1, 2, 'Fremhævet og lukket tråd', TRUE, TRUE);");
+			statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (1, 2, 'Fremhævet tråd', TRUE, FALSE);");
+			statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (3, 2, 'Lukket tråd', FALSE, TRUE);");
+			statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (5, 2, 'Normal tråd', FALSE, FALSE);");
+			
+			statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (1, 1, 'Tekst i første tråd.');");
+			statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (1, 2, 'Tekst i anden tråd.');");
+			statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (3, 3, 'Tekst i tredje tråd.');");
+			statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (5, 4, 'Tekst i fjerde tråd.');");
+			
+		} finally {
+			statement.close();	
+		}
 		
-		statement.executeUpdate("CREATE TABLE users (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
-													"name VARCHAR(20) NOT NULL UNIQUE, " +
-													"password VARBINARY(32) NOT NULL, " +
-													"type INTEGER NOT NULL, " +
-													"PRIMARY KEY (identifier));");
+	}
+	
+	public List<User> getUsers() throws Exception {
 		
-		statement.executeUpdate("CREATE TABLE categories (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
-														 "name VARCHAR(100) NOT NULL, " +
-														 "parent INTEGER, " +
-														 "PRIMARY KEY (identifier), " +
-														 "UNIQUE (name, parent), " +
-														 "FOREIGN KEY (parent) REFERENCES categories(identifier));");
+		Statement statement = null;
+		ResultSet resultSet = null;
 		
-		statement.executeUpdate("CREATE TABLE threads (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
-													  "user INTEGER NOT NULL, " +
-													  "category INTEGER NOT NULL, " + 
-													  "name VARCHAR(100) NOT NULL, " +
-													  "sticky BOOLEAN NOT NULL, " +
-													  "closed BOOLEAN NOT NULL, " +
-													  "created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-													  "PRIMARY KEY (identifier), " +
-													  "FOREIGN KEY (user) REFERENCES users(identifier), " +
-													  "FOREIGN KEY (category) REFERENCES categories(identifier));");
+		try {
+			
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM users;");
+			List<User> list = new ArrayList<User>();
+
+			while (resultSet.next()) {
+				list.add(new User(this, resultSet.getInt("identifier"), resultSet.getString("mail"), resultSet.getString("name"), resultSet.getBytes("password"), resultSet.getInt("type")));
+			}
+			
+			return list;
+			
+		} finally {
+			resultSet.close();
+			statement.close();
+		}
 		
-		statement.executeUpdate("CREATE TABLE comments (identifier INTEGER NOT NULL AUTO_INCREMENT, " +
-													   "user INTEGER NOT NULL, " +
-													   "thread INTEGER NOT NULL, " +
-													   "content VARCHAR(1000) NOT NULL, " +
-													   "changed TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
-													   "PRIMARY KEY (identifier), " +
-													   "FOREIGN KEY (user) REFERENCES users(identifier), " +
-													   "FOREIGN KEY (thread) REFERENCES threads(identifier));");
-		
-		// Insert		
-		
-		statement.executeUpdate("INSERT INTO users (name, password, type) VALUES ('Administrator', 'a', 0);");
-		statement.executeUpdate("INSERT INTO users (name, password, type) VALUES ('Moderator', 'm', 1);");
-		statement.executeUpdate("INSERT INTO users (name, password, type) VALUES ('Bruger', 'b', 2);");
-		
-		statement.executeUpdate("INSERT INTO categories (name, parent) VALUES ('Hovedkategori', NULL);");
-		statement.executeUpdate("INSERT INTO categories (name, parent) VALUES ('Underkategori', 1);");
-		
-		statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (1, 2, 'Fremhævet og lukket tråd', TRUE, TRUE);");
-		statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (1, 2, 'Fremhævet tråd', TRUE, FALSE);");
-		statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (3, 2, 'Lukket tråd', FALSE, TRUE);");
-		statement.executeUpdate("INSERT INTO threads (user, category, name, sticky, closed) VALUES (5, 2, 'Normal tråd', FALSE, FALSE);");
-		
-		statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (1, 1, 'Tekst i første tråd.');");
-		statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (1, 2, 'Tekst i anden tråd.');");
-		statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (3, 3, 'Tekst i tredje tråd.');");
-		statement.executeUpdate("INSERT INTO comments (user, thread, content) VALUES (5, 4, 'Tekst i fjerde tråd.');");
+	}
+	
+	protected void createUser() {
 		
 	}
 	
@@ -125,11 +162,12 @@ public class Connector {
 		PreparedStatement statement = null;
 		
 		try {
-			statement = connection.prepareStatement("UPDATE users SET name = ?, password = ?, type = ? WHERE identifier = ?;");
-			statement.setString(1, user.getName());
-			statement.setBytes(2, user.getPassword());
-			statement.setInt(3, user.getType().getValue());
-			statement.setInt(4, user.getIdentifier());
+			statement = connection.prepareStatement("UPDATE users SET mail = ?, name = ?, password = ?, type = ? WHERE identifier = ?;");
+			statement.setString(1, user.getMail());
+			statement.setString(2, user.getName());
+			statement.setBytes(3, user.getPassword());
+			statement.setInt(4, user.getType());
+			statement.setInt(5, user.getIdentifier());
 			statement.executeUpdate();
 		} finally {
 			statement.close();
