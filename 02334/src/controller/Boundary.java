3679cc8aa5fdba6d0d48b7f582a6457dcf4d00be
Emil <Emil@10.16.104.239>
2013-10-02 03:50:07 -0700

    fjernet selvlavet hash :)
    Signed-off-by: Emil <Emil@10.16.104.239>

--- 02334/src/controller/Boundary.java
+++ 02334/src/controller/Boundary.java
@@ -6,31 +6,5 @@ public class Boundary {
 
 
 
-	// Check på password
-	public static String passCheck(String pass) {
-	
-		if (pass.length() > 24) {
-			return "text to long";
 
-		} else if (pass != null && pass.length() > 3) {
-			System.out.println("Før hash er pass: " + pass);
-			int hash = Boundary.passHash(pass);
-			System.out.println("Efter hash er pass blevet til: " + hash);
-			return pass;
-						
-		}else return "password ikke godkendt";
-		
-
-	}
-
-	// hash-creater. Creates hash of the given pass
-	public static int passHash(String pass) {
-		int hash = 7;
-		for (int i = 0; i < pass.length(); i++) {
-			hash = hash * 31 + pass.hashCode() * pass.hashCode();
-		
-		}
-
-		return hash;
-	}
 }
