b8ce18a38a8fa18d4123d82d08f27af03871221a
khannoori <khan_noori@msn.com>
2013-10-03 10:01:42 +0200

    Session

---
+++ 02334/src/Web/Session.java
@@ -0,0 +1,15 @@
+package Web;
+
+import database.Connector;
+import database.User;
+
+public class Session {
+	private static Connector connector;
+	private User user = null;
+
+	public Session() {
+		if (connector == null){
+			
+		}
+	}
+}
